f22202070b143b2179be8beed921273dc550a956
Глеб Толстенко <gvtolstenko@edu.hse.ru>
2017-09-06 00:05:47 +0500

    Код к 4 заданию

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("epsilon vvedite ");
+            double epsilon = 0;
+
+            while (!Double.TryParse(Console.ReadLine(), out epsilon))
+                Console.WriteLine("nevernii vvod");
+
+            double ans = 1/2;
+            int i = 2;
+            double slag = ans;
+
+            while (slag < epsilon)
+            {
+                slag = i * (i + 1);
+                slag = 1 / slag;
+                ans += slag;
+                i++;
+            }
+
+            Console.WriteLine("otvet " + ans);
+            Console.ReadKey();
+        }
+    }
+}
